8f863a8194d7e8bcac426e5accb93ad20dc67362
Sven Dubbeld <sven.dubbeld1@gmail.com>
2016-09-07 17:07:20 +0200

    Rename elastAlert to elastalert

--- src/common/config/schema.js
+++ src/common/config/schema.js
@@ -6,7 +6,7 @@ import Joi from 'joi';
 const schema = Joi.object().keys({
   'appName': Joi.string().default('elastalert-server'),
   'port': Joi.number().default(3030),
-  'elastAlertPath': Joi.string().default('/opt/elastalert')
+  'elastalertPath': Joi.string().default('/opt/elastalert')
 });
 
 export default schema;
