jesesun/elastalert-1

Refactored bitsensor enhancement

elastalert_modules/bitsensor_enhancement.py
@@ -1,12 +1,13 @@
 from elastalert.enhancements import BaseEnhancement
 from util import convert_array_to_object
+from util import parse_object
 
-
+# For easier access to nested values in an array , this merges all items in  array
+# within  
 class AlertTextEnhancement(BaseEnhancement):
     # The enhancement is run against every match
     # The match is passed to the process function where it can be modified in any way
     # ElastAlert will do this for each enhancement linked to a rule
     def process(self, match):
-        match['detections_string'] = ''
-        if 'detections' in match:
-            match['detections_parsed'] = convert_array_to_object(match['detections'])
+        parsed_match = parse_object(match)
+        match.update(parsed_match)

bitsensor/elastalert

elastalert_modules/bitsensor_enhancement.py
@@ -1,12 +1,13 @@
 from elastalert.enhancements import BaseEnhancement
 from util import convert_array_to_object
+from util import parse_detections
 
-
+# For easier access to nested values in an array , this merges all items in  array
+# within  
 class AlertTextEnhancement(BaseEnhancement):
     # The enhancement is run against every match
     # The match is passed to the process function where it can be modified in any way
     # ElastAlert will do this for each enhancement linked to a rule
     def process(self, match):
-        match['detections_string'] = ''
-        if 'detections' in match:
-            match['detections_parsed'] = convert_array_to_object(match['detections'])
+        parsed_match = parse_detections(match)
+        match.update(parsed_match)