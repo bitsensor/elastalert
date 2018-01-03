c0d9a04fdec55b440673d6c78ff3b38d8a5cb530
Martijn Rondeel <martijn@rondeel.email>
2018-01-03 18:11:25 +0100

    Capture unhandled rejections

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import ElastalertServer from './elastalert_server';
 
 if (process.env.BITSENSOR_SENTRY_ENABLED !== 'false' && process.env.BITSENSOR_SENTRY_DSN !== undefined) {
   var Raven = require('raven');
-  Raven.config(process.env.BITSENSOR_SENTRY_DSN).install(console.log('Sentry logging enabled for Elastalert'));
+  Raven.config(process.env.BITSENSOR_SENTRY_DSN, {
+    captureUnhandledRejections: true
+  }).install();
+  console.log('Sentry logging enabled for Elastalert');
 
   Raven.context(function () {
     let server = new ElastalertServer();
