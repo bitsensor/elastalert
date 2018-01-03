d8a9d9fdcbd4e17ac431bc280eebd6e0fbf744e6
Martijn Rondeel <martijn@rondeel.email>
2018-01-03 16:33:00 +0100

    Implement Sentry logging

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import ElastalertServer from './elastalert_server';
 
-let server = new ElastalertServer();
-server.start();
+if (process.env.BITSENSOR_SENTRY_ENABLED !== 'false') {
+  var Raven = require('raven');
+  Raven.config('http://527ee316013644b387a47f713335b085:67f9631c6e1043e3abe43c72364f4d1b@dev.bitsensor.xyz:9000/5').install();
+
+  Raven.context(function () {
+    let server = new ElastalertServer();
+    server.start();
+  });
+} else {
+  let server = new ElastalertServer();
+  server.start();
+}
