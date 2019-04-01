ad03913a1faf07b36e36297d67bfd10211ec494e
Garrett Thornburg <film42@gmail.com>
2019-04-01 10:56:09 -0400

    Add a callback to the elastalert process to stop the server on exit

--- src/controllers/process/index.js
+++ src/controllers/process/index.js
@@ -9,6 +9,7 @@ export default class ProcessController {
 
   constructor() {
     this._elastalertPath = config.get('elastalertPath');
+    this._onExitCallbacks = [];
     this._status = Status.IDLE;
 
     /**
@@ -16,6 +17,10 @@ export default class ProcessController {
      * @private
      */
     this._process = null;
+  }
+
+  onExit(onExitCallback) {
+    this._onExitCallbacks.push(onExitCallback);
   }
 
   get status() {
@@ -112,6 +117,12 @@ export default class ProcessController {
         this._status = Status.ERROR;
       }
       this._process = null;
+
+      this._onExitCallbacks.map(function(onExitCallback) {
+        if (onExitCallback !== null) {
+          onExitCallback();
+        }
+      });
     });
 
     // Set listener for ElastAlert error
